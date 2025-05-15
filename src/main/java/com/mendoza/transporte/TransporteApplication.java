21371d1df8331755eefa58e3f3de500f20cf88f3
antonyCandiotti54321 <antonycm54321@gmail.com>
2025-05-14 23:58:01 -0500

    probando con create 2

--- src/main/java/com/mendoza/transporte/TransporteApplication.java
+++ src/main/java/com/mendoza/transporte/TransporteApplication.java
@@ -6,8 +6,6 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = "com.mendoza.transporte")
-@EntityScan(basePackages = "com.mendoza.transporte")
 public class TransporteApplication {
 
 	public static void main(String[] args) {
