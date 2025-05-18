4b571c0250a7cac7d64e62d07b52b54aa0c2caea
antonyCandiotti54321 <antonycm54321@gmail.com>
2025-05-18 00:46:30 -0500

    permitir ws

--- src/main/java/com/mendoza/transporte/config/SecurityConfig.java
+++ src/main/java/com/mendoza/transporte/config/SecurityConfig.java
@@ -55,6 +55,7 @@ public class SecurityConfig {
                         .requestMatchers("/api/empleado/**").hasAnyRole("ADMIN", "CHOFER")
                         .requestMatchers("/api/admin/**").hasRole("ADMIN")
                         .requestMatchers("/api/chofer/**").hasAnyRole("ADMIN", "CHOFER")
+                        .requestMatchers("/ws/**").hasRole("CHOFER")
                         // resto debe autenticarse
                         .anyRequest().authenticated()
                 )
