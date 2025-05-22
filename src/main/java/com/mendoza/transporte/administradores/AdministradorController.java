b7140acf0394d341f95b7510893573913c841acf
antonyCandiotti54321 <antonycm54321@gmail.com>
2025-05-22 04:05:55 -0500

    falta validar eunmvalidator

--- src/main/java/com/mendoza/transporte/administradores/AdministradorController.java
+++ src/main/java/com/mendoza/transporte/administradores/AdministradorController.java
@@ -2,6 +2,8 @@ package com.mendoza.transporte.administradores;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -24,19 +26,19 @@ public class AdministradorController {
 
     @Operation(summary = "Obtener administrador por ID", description = "Devuelve los datos de un administrador específico.")
     @GetMapping("/{id}")
-    public ResponseEntity<AdministradorResponse> getAdminById(@PathVariable Long id) {
+    public ResponseEntity<AdministradorResponse> getAdminById(@PathVariable @Positive(message = "El id debe ser positivo") Long id) {
         return ResponseEntity.ok(administradorService.getById(id));
     }
 
     @Operation(summary = "Actualizar administrador", description = "Actualiza los datos de un administrador específico.")
     @PutMapping("/{id}")
-    public ResponseEntity<AdministradorResponse> updateAdmin(@PathVariable Long id, @RequestBody AdministradorRequest request) {
+    public ResponseEntity<AdministradorResponse> updateAdmin(@PathVariable @Positive(message = "El id debe ser positivo") Long id,@Valid @RequestBody AdministradorRequest request) {
         return ResponseEntity.ok(administradorService.update(id, request));
     }
 
     @Operation(summary = "Eliminar administrador", description = "Elimina un administrador por su ID.")
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteAdmin(@PathVariable Long id) {
+    public ResponseEntity<Void> deleteAdmin(@PathVariable @Positive(message = "El id debe ser positivo") Long id) {
         administradorService.delete(id);
         return ResponseEntity.noContent().build();
     }
